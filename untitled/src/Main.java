a9df242e953a09375feea5275a90c0d3770be0be
DERGON OKAY <dergonokay@gmail.com>
2020-10-30 19:39:35 -0300

    Muevo el proyecto a al root

---
+++ untitled/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> lista = Arrays.asList(1, 2 ,3 ,4 ,5 ,6 ,7 ,87);
+
+        for(Integer i: lista) {
+            System.out.println(i);
+        }
+    }
+}
