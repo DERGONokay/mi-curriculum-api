c11504fc63e16848cea3d49a89f6d9ababea11e8
DERGON OKAY <dergonokay@gmail.com>
2021-01-21 22:46:02 -0300

    Optimize imports

--- src/main/kotlin/com/dergon/studios/mycv/api/controller/download/RegisterDownloadController.kt
+++ src/main/kotlin/com/dergon/studios/mycv/api/controller/download/RegisterDownloadController.kt
@@ -2,7 +2,10 @@ package com.dergon.studios.mycv.api.controller.download
 
 import com.dergon.studios.mycv.api.action.download.RegisterDownload
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/download/register")
@@ -13,6 +16,7 @@ class DownloadController(private val registerDownload: RegisterDownload) {
         registerDownload(request.email, request.docType)
     }
 }
+
 
 data class RegisterDownloadRequest(@JsonProperty("email") val email: String,
                                    @JsonProperty("docType") val docType: String)
