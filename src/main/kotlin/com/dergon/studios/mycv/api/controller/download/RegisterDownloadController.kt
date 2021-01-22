ffdde9d3af921f04f1249676203cd4bf54bdc60e
DERGON OKAY <dergonokay@gmail.com>
2021-01-22 01:15:47 -0300

    Add CORS to controllers

--- src/main/kotlin/com/dergon/studios/mycv/api/controller/download/RegisterDownloadController.kt
+++ src/main/kotlin/com/dergon/studios/mycv/api/controller/download/RegisterDownloadController.kt
@@ -2,13 +2,11 @@ package com.dergon.studios.mycv.api.controller.download
 
 import com.dergon.studios.mycv.api.action.download.RegisterDownload
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/download/register")
+@CrossOrigin(origins = ["https://damian-lisas-cv.firebaseapp.com/"])
 class RegisterDownloadController(private val registerDownload: RegisterDownload) {
 
     @PostMapping
